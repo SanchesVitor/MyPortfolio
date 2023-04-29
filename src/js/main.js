57c648d08dd4bc542f1829b6a37e12ed0ca77d41
SanchesVitor <vitorsanches06@hotmail.com>
2023-04-29 00:58:44 -0300

    updated html, styles and scripts, work and portfolio

--- src/js/main.js
+++ src/js/main.js
@@ -47,3 +47,23 @@ function activeWork() {
 }
 
 linkWork.forEach(l => l.addEventListener('click', activeWork));
+
+/* Work Popup */
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('work-button')) {
+    togglePortfolioPopup();
+    portfolioItemDetails(e.target.parentElement);
+  }
+});
+
+function togglePortfolioPopup() {
+  document.querySelector('.portfolio-popup').classList.toggle('open');
+}
+
+document.querySelector('.portfolio-popup-close').addEventListener('click', togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem) {
+  document.querySelector('.pp-thumbnail img').src = portfolioItem.querySelector('.work-img').src;
+  document.querySelector('.portfolio-popup-subtitle span').innerHTML = portfolioItem.querySelector('.work-title').innerHTML;
+  document.querySelector('.portfolio-popup-body').innerHTML = portfolioItem.querySelector('portfolio-item-details').innerHTML;
+}
