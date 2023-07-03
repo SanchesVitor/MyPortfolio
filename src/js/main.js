1383a3f864fa565fa3bfa3f64dabf88c0e2b3ace
SanchesVitor <vitorsanches06@hotmail.com>
2023-07-03 06:48:48 -0300

    Add script Services

--- src/js/main.js
+++ src/js/main.js
@@ -71,4 +71,22 @@ function portfolioItemDetails(portfolioItem) {
 /* SERVICES MODAL */
 const modalViews = document.querySelectorAll('.services-modal'),
       modelBtns = document.querySelectorAll('.services-button'),
-      modalCloses = document.querySelectorAll('.services-modal-close')
+      modalCloses = document.querySelectorAll('.services-modal-close');
+
+let modal = function(modalClick) {
+  modalViews[modalClick].classList.add('active-modal');
+}
+
+modelBtns.forEach((modelBtn, i) => {
+  modelBtn.addEventListener('click', () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener('click', () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove('active-modal');
+    });
+  });
+});
